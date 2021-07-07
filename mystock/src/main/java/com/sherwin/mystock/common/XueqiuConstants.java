9c175b741b61c872497b8287f84cba7c2117ef4f
zhuangxuewei <zhuangxuewei@192.168.10.7>
2021-07-07 22:19:15 +0800

    雪球api

---
+++ mystock/src/main/java/com/sherwin/mystock/common/XueqiuConstants.java
@@ -0,0 +1,9 @@
+package com.sherwin.mystock.common;
+
+public class XueqiuConstants {
+	public static String XUEQIU_STOCK_DOMAIN = "https://stock.xueqiu.com";
+	public static String XUEQIU_DOMAIN = "https://xueqiu.com";
+	public static String LIST_INFO = "/service/v5/stock/screener/quote/list?size=4475&order=desc&orderby=percent&order_by=percent&market=CN&type=sh_sz&_=1625663708966";
+
+	
+}
--- mystock/src/test/java/com/sherwin/mystock/StockTest.java
+++ mystock/src/test/java/com/sherwin/mystock/StockTest.java
@@ -7,27 +7,38 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
-class StockTest {
-	@Autowired
-	RestTemplate restTemplate;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.sherwin.mystock.common.XueqiuConstants;
+
+public class StockTest {
+//	@Autowired
+//	RestTemplate restTemplate;
 	private String XUEQIU_URL = "https://stock.xueqiu.com/v5/stock/";
 
 	@Test
-	void test() throws ClientProtocolException, IOException {
+	public void test() throws ClientProtocolException, IOException {
 		String realtimeQuote = XUEQIU_URL + "realtime/quotec.json?symbol=SH601318,SH688981,SZ000333,SH601665,SZ002185";
+		String listQuote=XueqiuConstants.XUEQIU_DOMAIN+XueqiuConstants.LIST_INFO;
 		HttpClientBuilder builder = HttpClientBuilder.create();
 		CloseableHttpClient client = builder.build();
-		HttpGet httpGet = new HttpGet(realtimeQuote);
+		HttpGet httpGet = new HttpGet(listQuote);
 
 		BasicResponseHandler handler = new BasicResponseHandler();
 		String response = client.execute(httpGet, handler);
-		System.out.println(response);
-//		https: // stock.xueqiu.com/v5/stock/realtime/quotec.json?symbol=SH601003,SH601001
-//		param.put("symbol", "SH601003");
+		JSONObject resJson=JSONObject.parseObject(response);
+		JSONObject data =resJson.getJSONObject("data");
+		JSONArray list=data.getJSONArray("list");
+		System.out.println(list.size());
+//		System.out.println(response);
+		
+		// https: //
+		// stock.xueqiu.com/v5/stock/realtime/quotec.json?symbol=SH601003,SH601001
+		// param.put("symbol", "SH601003");
 	}
 
 }
